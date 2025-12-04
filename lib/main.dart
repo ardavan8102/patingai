e006e12c14170bc31692b4b786d7bf4511cb78b9
ardavan8102 <eskandariardavan81@gmail.com>
2025-12-04 18:34:48 +0330

    Finished: created Api With Python/Flask

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:pating_ai/consts/app_routes.dart';
 import 'package:pating_ai/consts/app_theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
