892102d134acea98c70abcde8bf4f016be9ebbb4
ardavan8102 <eskandariardavan81@gmail.com>
2025-12-04 18:49:36 +0330

    Finished: Api Service => Flutter

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,53 @@
+import 'dart:convert';
+import 'package:pating_ai/models/message_model.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
   
+  final String baseURL = ' https://patingai.ir/api';
+
+  Future<Map<String, dynamic>> sendMessage ({
+    required String message,
+    List<MessageModel>? conversation
+  }) async {
+    List<Map<String, dynamic>> convList = [];
+
+    if (conversation != null) {
+      List<MessageModel> conversationForApi = List.from(conversation);
+
+      if (conversationForApi.isNotEmpty && conversationForApi.last.isFromUser) {
+        conversationForApi.removeLast();
+      }
+
+      for(var msg in conversationForApi){
+        convList.add(
+          {
+            "role" : msg.isFromUser ? "user" : "assistant",
+            "content" : msg.text
+          }
+        );
+      }
+    }
+
+    final url = Uri.parse("$baseURL/ai");
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type' : 'application/json'},
+      body: jsonEncode(
+        {
+          "message" : message,
+          "conversation" : convList,
+        }
+      )
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return {'error' : 'no valid response from api'};
+    }
+
+  }
+
 }
